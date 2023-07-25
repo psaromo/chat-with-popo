18a36ba7fdebd7f946e91875733759b971f72b7a
Pohla Saromo <saromopohla@gmail.com>
2023-07-25 09:53:16 -0400

    Prepare home buttons

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from "next/head";
-import Link from "next/link";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import Head from 'next/head';
+import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { getSession } from '@auth0/nextjs-auth0';
 
 export default function Home() {
   const { isLoading, error, user } = useUser();
@@ -9,15 +10,53 @@ export default function Home() {
   if (error) return <div>{error.message}</div>;
 
   return (
-    <div>
+    <>
       <Head>
         <title>Chat with Popo</title>
       </Head>
-      <h1>Chat with Popo</h1>
-      <div>
-        {!!user && <Link href="/api/auth/logout">Logout</Link>}
-        {!user && <Link href="/api/auth/login">Login</Link>}
+      <div className="flex min-h-screen w-full items-center justify-center bg-gray-800 text-center text-white">
+        <div>
+          {!!user && (
+            <Link
+              href="/api/auth/logout"
+              className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+            >
+              Logout
+            </Link>
+          )}
+          {!user && (
+            <>
+              <Link
+                href="/api/auth/login"
+                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+              >
+                Login
+              </Link>
+              <Link
+                href="/api/auth/signup"
+                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+              >
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx.req, ctx.res);
+  if (!!session) {
+    return {
+      redirect: {
+        destination: '/chat',
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
