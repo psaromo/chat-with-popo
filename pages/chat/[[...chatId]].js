be64af40ea2a018b646bb5c23fafd697ccc08cfc
Pohla Saromo <saromopohla@gmail.com>
2023-07-25 15:22:27 -0400

    OpenAI setup

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,12 +1,57 @@
-import Head from "next/head";
+import Head from 'next/head';
+import { ChatSidebar } from 'pages/components/ChatSidebar';
+import { useState } from 'react';
 
-export default function Home() {
+export default function ChatPage() {
+  const [messageText, setMessageText] = useState('');
+  const [incomingMessage, setIncomingMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`/api/chat/sendMessage`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ message: messageText }),
+    });
+
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    await streamReader(reader, (message) => {
+      console.log(message);
+      setIncomingMessage((s) => `${s}${message.content}`);
+    });
+  };
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>New chat</title>
       </Head>
-      <h1>Welcome to the ChatId page</h1>
-    </div>
+      <div className="grid h-screen grid-cols-[260px_1fr]">
+        <ChatSidebar />
+        <div className="flex flex-col bg-gray-700">
+          <div className="flex-1 text-white">{incomingMessage}</div>
+          <footer className="bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+              <fieldse className="flex gap-2">
+                <textarea
+                  value={messageText}
+                  onChange={(e) => setMessageText(e.target.value)}
+                  placeholder="Send a message..."
+                  className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
+                />
+                <button type="submit" className="btn">
+                  Send
+                </button>
+              </fieldse>
+            </form>
+          </footer>
+        </div>
+      </div>
+    </>
   );
 }
